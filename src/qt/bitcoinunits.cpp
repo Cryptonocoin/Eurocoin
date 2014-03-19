0ab723ee00a2e5f84af03ae826b88c78f1fe6f64
Eurocoin-EUC <coin@eurocoin.info>
2014-03-19 16:44:35 +0100

    EuroCoin

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("GEO");
-    case mBTC: return QString("mGEO");
-    case uBTC: return QString::fromUtf8("μGEO");
+    case BTC: return QString("EUC");
+    case mBTC: return QString("mEUC");
+    case uBTC: return QString::fromUtf8("μEUC");
     default: return QString("???");
     }
 }
